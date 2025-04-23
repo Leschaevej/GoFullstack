3c64fac4bb789f7e7b7c349e6a25243e6f2fa5d6
Jimmy Leschaeve <leschaeve.jimmy@gmail.com>
2025-04-23 13:16:33 +0200

    0.2

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const stuffRoutes = require('./routes/stuff');
+
+mongoose.connect('mongodb+srv://leschaevejimmy:FreeDoMind1996fdm!@cluster.h0j3cus.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster',
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion réussie à la library !'))
+    .catch(() => console.log('Connexion échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
-  console.log('Requête reçue !');
-  next();
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
 });
 
-app.use((req, res, next) => {
-  res.status(201);
-  next();
-});
+app.use(bodyParser.json());
 
-app.use((req, res, next) => {
-  res.json({ message: 'Votre requête a bien été reçue !' });
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log('Réponse envoyée avec succès !');
-});
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
