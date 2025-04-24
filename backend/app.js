a83efbae19739530e8175ca185dd4b3fb333f4a6
Jimmy Leschaeve <leschaeve.jimmy@gmail.com>
2025-04-24 15:35:50 +0200

    0.4

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const stuffRoutes = require('./routes/stuff');
 const userRoutes = require('./routes/user');
+const path = require('path');
 
 mongoose.connect('mongodb+srv://leschaevejimmy:FreeDoMind1996fdm!@cluster.h0j3cus.mongodb.net/library?retryWrites=true&w=majority&appName=Cluster')
     .then(() => console.log('Connexion réussie à la library !'))
@@ -22,5 +23,7 @@ app.use(bodyParser.json());
 
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
 
 module.exports = app;
